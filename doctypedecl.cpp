d226d8c002274d1d7b21147230ef5257203f36c8
Mohammed Elalj <mohammed@elalj.com>
2014-04-11 11:32:55 +0200

    Doctype constructor

--- doctypedecl.cpp
+++ doctypedecl.cpp
@@ -1,6 +1,24 @@
 #include "doctypedecl.h"
 
 
+Doctypedecl::Doctypedecl(char * root_element) {
+    this->root_element = root_element;
+}
+
+Doctypedecl::Doctypedecl(char * root_element, char * external_id, char * dtd_name) {
+    this->root_element = root_element;
+    this->external_id = external_id;
+    this->dtd_name = dtd_name;
+}
+Doctypedecl::Doctypedecl(char * root_element, char * external_id, char * dtd_name, char * dtd_location) {
+	this->root_element = root_element;
+	this->external_id = external_id;
+	this->dtd_name = dtd_name;
+	this->dtd_location = dtd_location;
+}
+
+
+
 Doctypedecl::Doctypedecl() {
     
 }
