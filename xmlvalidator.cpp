aafcb6fe73e3486003b279e986fe547114d01e4a
Kim CHOUARD <kim.chouard@gmail.com>
2014-04-16 15:05:47 +0200

    xsd call the right regexcreator

--- xmlvalidator.cpp
+++ xmlvalidator.cpp
@@ -15,9 +15,21 @@ void Xmlvalidator::mapsCreate(Document *xsd) {
 		for(vector<Item *>::iterator itElem = children.begin(); itElem != children.end(); itElem++) {
 			// Get the actual element name
             string name = ((Tag *) (*itElem))->getAtts().front()->value;
-
             cout << "Get ready for the regex of " << name << endl;
-            // mapRegex.insert(pair<string, string>(name, regex));
+
+            // Get the contruction node
+            Tag * type = (Tag *) ((Tag *) (*itElem))->getChildren().front();
+            Tag * construction = (Tag *) type->getChildren().front();
+            cout << "Will create a regex for " << construction->getName() << endl;
+
+            if (construction->getName() == "xsd:choice")
+            {
+            	choiceRegex(construction);
+            }
+            else if (construction->getName() == "xsd:sequence")
+            {
+            	seqRegex(construction);
+            }
         }
 		// TODO
 	}
@@ -25,6 +37,17 @@ void Xmlvalidator::mapsCreate(Document *xsd) {
 	{
 		cout << "Error: Wrong syntax for the given xsd while creating the maps." << endl;
 	}
+}
+
+void Xmlvalidator::choiceRegex(Tag* choice) {
+	cout << "Processing a choice complexType " << endl;
+	// TODO
+    // mapRegex.insert(pair<string, string>(name, regex));
+}
+
+void Xmlvalidator::seqRegex(Tag* seq) {
+	cout << "Processing a sequence complexType " << endl;
+	// TODO
 }
 
 void Xmlvalidator::validityCheck(Document *xml) {
--- xmlvalidator.h
+++ xmlvalidator.h
@@ -12,6 +12,8 @@ class Xmlvalidator {
 private:
     map<string, string> mapType;
     map<string, string> mapRegex;
+    void choiceRegex(Tag* choice);
+    void seqRegex(Tag* seq);
 public:
     Xmlvalidator();
     ~Xmlvalidator();
