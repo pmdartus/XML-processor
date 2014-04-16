b2ce541f7515a44eb73067ebdec340d4abab0c15
Kim CHOUARD <kim.chouard@gmail.com>
2014-04-16 14:51:12 +0200

    xsd child looping

--- xmlvalidator.cpp
+++ xmlvalidator.cpp
@@ -7,8 +7,18 @@ void Xmlvalidator::mapsCreate(Document *xsd) {
 	// Check that the root is a schema
 	if (xsd->getRoot()->getName() == "xsd:schema")
 	{
-		vector<Item*> children = xsd->getRoot()->getChildren();
 		cout << "Start creating the maps" << endl;
+
+		// Get all the childrens
+		vector<Item*> children = xsd->getRoot()->getChildren();
+
+		for(vector<Item *>::iterator itElem = children.begin(); itElem != children.end(); itElem++) {
+			// Get the actual element name
+            string name = ((Tag *) (*itElem))->getAtts().front()->value;
+
+            cout << "Get ready for the regex of " << name << endl;
+            // mapRegex.insert(pair<string, string>(name, regex));
+        }
 		// TODO
 	}
 	else
